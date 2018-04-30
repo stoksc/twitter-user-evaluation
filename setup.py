7b4c4d0d40e887c96ef3068f9a160ef00a348ec8
Bradley Laney <bradley.laney@gmail.com>
2018-04-29 20:30:51 -0400

    update dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     packages=['twitter_user_evaluation', 'twitter_user_evaluation/tools'],
     include_package_data=True,
     install_requires=[
+        'tensorflow',
         'flask',
         'gensim',
         'keras',
