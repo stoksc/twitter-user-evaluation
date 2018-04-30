e64bf1d30efbe2ba84029a7f50a3076acdc15809
Bradley Laney <bradley.laney@gmail.com>
2018-04-29 20:53:46 -0400

    jesus travisci

--- twitter_user_evaluation/tools/analytics.py
+++ twitter_user_evaluation/tools/analytics.py
@@ -30,6 +30,7 @@ nltk.download('vader_lexicon')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
+nltk.download('punkt')
 MODEL = SentimentIntensityAnalyzer()
 
 # keras political sentiment model
